f23fa1f2e18b6420fa2d00ee740a8d73651c051e
PetarDDobrev <petarddobrev@abv.bg>
2014-02-22 01:55:42 +0200

    added documentation of the ship class

--- Battleship/bin/ship.rb
+++ Battleship/bin/ship.rb
@@ -3,16 +3,21 @@ require_relative 'constants'
 #ship class
 class Ship
   attr_accessor :cordinates, :size, :states
+  ##creates ship object
   #ship object is initialized with size of 1,2,3,4
-  #cordinates is array of the cells which are occupied by the ship 
+  #cordinates is array of the cells which are occupied by the ship
+  #states is array of contitions of each cordinate Dead/Alive
   def initialize(size)
     @cordinates = Array.new(size,0)
     @states = Array.new(size,ALIVE)
     @size = size
   end
 
+  ##sets the position of teh ship
+  #head and tail are cordinates to the to ends of the ship,
+  #for instance 'A2', 'D5'
+  #if the ship is size 1 head == tail  
   def set_position(head, tail)
-    #return false if head.length > 3 or tail.size > 3
     head_position = head.split(//,2)
     tail_position = tail.split(//,2)
     return false if not correct_positions(head_position, tail_position)
@@ -26,6 +31,10 @@ class Ship
     true
   end
 
+  ##sets the positions from head to tail
+  #here head and tail are arrays of type ['A','2'], ['D','10']
+  #for example head=['A','1'], tail=['A','4'] the ship.cordinates
+  #will be ['A1','A2','A3','A4'] 
   def set_head_to_tail(head, tail)
     if head[0] == tail[0]
       numbers = (0..@size-1).to_a
@@ -36,6 +45,10 @@ class Ship
     end
   end
 
+  ##sets the positions from tail to head
+  #here head and tail are arrays of type ['A','2'], ['D','10']
+  #for example head=['A','1'], tail=['A','4'] the ship.cordinates
+  #will be ['A4','A3','A2','A1']
   def set_tail_to_head(head, tail)
     if head[0] == tail[0]
       numbers = (0..@size-1).to_a
@@ -46,6 +59,11 @@ class Ship
     end
   end
 
+  ##checks if the given head and tail are correct
+  #here head and tail are cordinates like 'A1','B1' and so on
+  #if head or tail is 'Q1' or 'A11' it will return false
+  #also if head and tail are not in one horizontal or one vertical
+  #the function will return false
   def correct_positions(head, tail)
     cordiantes = (head.zip tail).flatten
     chars_allowed = (BOARD_ALPHABET.zip BOARD_NUMBERS).flatten
@@ -56,16 +74,22 @@ class Ship
     true
   end
 
+  #checks if the ship is dead: if all of his cordinates are hit,
+  #by checking @states
   def dead?
     return true if @states.reduce(:+) == 0
     false
   end
 
+  #checks if the ship has been hit at least one time, by checking
+  #@states
   def hit?
-    return true if @states.reduce(:+) < @size
+     return true if @states.reduce(:+) < @size
     false
   end
 
+  ##check if the given cell is one of the ships cordinates
+  #and if it is it 'hits' it
   def hit(cell)
     cell_index = @cordinates.index(cell)
     @states[cell_index] = DEAD unless cell_index == nil
